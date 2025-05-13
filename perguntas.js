9b5d212803b5a8e6509b50b0bc1bffda646e2e32
britosu <brito.suellen@escola.pr.gov.br>
2025-05-13 14:28:17 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'Linguagem de marcação'
+)
+
+criaCartao(
+    'Ingles',
+    'Como é o dia do pagamento',
+    'Payday'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
